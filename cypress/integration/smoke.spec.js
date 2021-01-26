df83ff090e9816ffac7cb80f06a726d01933bbe6
Bill D <wpdnomad@yahoo.com>
2021-01-26 09:05:14 -0800

    Add environment variable for breaking set-env command in test.yml

--- cypress/integration/smoke.spec.js
+++ cypress/integration/smoke.spec.js
@@ -1,6 +1,6 @@
-describe("Smoke Test", () => {
-  it("can view the home page", () => {
-    cy.visit("/");
-    cy.contains("Learn React");
+describe('Smoke Test', () => {
+  it('can view the home page', () => {
+    cy.visit('/');
+    cy.contains('Learn React');
   });
 });

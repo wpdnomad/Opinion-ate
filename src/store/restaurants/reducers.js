71e255360e87e4e31a55da57592e1df856a77b88
Bill D <wpdnomad@yahoo.com>
2021-01-27 10:20:23 -0800

    Fix prettier crap 2

--- src/store/restaurants/reducers.js
+++ src/store/restaurants/reducers.js
@@ -2,7 +2,6 @@ import {combineReducers} from 'redux';
 import {STORE_RESTAURANTS} from './actions';
 
 const records = (state = [], action) => {
-  
   switch (action.type) {
     case STORE_RESTAURANTS:
       return action.records;

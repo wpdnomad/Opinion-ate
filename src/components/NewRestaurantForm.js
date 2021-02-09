20591cc7636f8b8b3f525b34f5906ec91f3dc264
Bill D <wpdnomad@yahoo.com>
2021-02-09 12:34:59 -0800

    Added test for does not display a validation error under When filled in describe

--- src/components/NewRestaurantForm.js
+++ src/components/NewRestaurantForm.js
@@ -2,18 +2,22 @@ import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Alert from '@material-ui/lab/Alert';
 import {createRestaurant} from '../store/restaurants/actions';
 
 export const NewRestaurantForm = ({createRestaurant}) => {
   const [name, setName] = useState('');
+  const [validationError, setValidationError] = useState(false);
   const handleSubmit = e => {
     e.preventDefault();
+    setValidationError(true);
     createRestaurant(name).then(() => {
       setName('');
     });
   };
   return (
     <form onSubmit={handleSubmit}>
+      {validationError && <Alert severity="error">Name is required</Alert>}
       <TextField
         value={name}
         onChange={e => setName(e.target.value)}
--- src/components/__tests__/NewRestaurantForm.spec.js
+++ src/components/__tests__/NewRestaurantForm.spec.js
@@ -6,6 +6,7 @@ import flushPromises from 'flush-promises';
 
 describe('NewRestaurantForm', () => {
   const restaurantName = 'Sushi Place';
+  const requiredError = 'Name is required';
 
   let createRestaurant;
   let context;
@@ -13,6 +14,12 @@ describe('NewRestaurantForm', () => {
   beforeEach(() => {
     createRestaurant = jest.fn().mockName('createRestaurant');
     context = render(<NewRestaurantForm createRestaurant={createRestaurant} />);
+  });
+  describe('initially', () => {
+    it('does not display a validation error', () => {
+      const {queryByText} = context;
+      expect(queryByText(requiredError)).toBeNull();
+    });
   });
   describe('when filled in', () => {
     beforeEach(async () => {
@@ -33,6 +40,26 @@ describe('NewRestaurantForm', () => {
     it('clears the name', () => {
       const {getByPlaceholderText} = context;
       expect(getByPlaceholderText('Add Restaurant').value).toEqual('');
+    });
+    it('does not display a validation error', () => {
+      const {queryByText} = context;
+      expect(queryByText(requiredError)).toBeNull();
+    });
+  });
+  describe('when empty', () => {
+    beforeEach(async () => {
+      createRestaurant.mockResolvedValue();
+  
+      const {getByPlaceholderText, getByTestId} = context;
+      await userEvent.type(getByPlaceholderText('Add Restaurant'), '');
+      userEvent.click(getByTestId('new-restaurant-submit-button'));
+  
+      return act(flushPromises);
+    });
+  
+    it('displays a validation error', () => {
+      const {queryByText} = context;
+      expect(queryByText(requiredError)).not.toBeNull();
     });
   });
 });
